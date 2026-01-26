94f02eab3462c949ccda70b7775b842d590022ad
Alex Algarate <alex.algarate@gmail.com>
2026-01-26 20:52:44 +0100

    create product model

---
+++ product.js
@@ -0,0 +1,20 @@
+import mongoose, { mongo } from 'mongoose';
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // esto crea createdAt y updatedAt automáticamente en la db
+    timestamps: true,
+  },
+);
+
+export const Product = mongoose.model('Product', ProductSchema, 'Products');
