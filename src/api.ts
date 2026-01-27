2dd2ce61fe8a9c863586f1bfb3871b810f180e3c
Alex Algarate <alex.algarate@gmail.com>
2026-01-27 20:42:34 +0100

    refactor api

---
+++ src/api.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import productRouter from './product.routes';
+
+export const app = express();
+
+// Middleware para decirle a Express que los body serán JSON (método POST)
+app.use(express.json());
+
+app.use('/products', productRouter);
+app.use('/products/:productId', productRouter);
+
+export const startHTTPApi = (): void => {
+  app.listen(3000, () => {
+    console.log('Up & running on port: ', 3000);
+  });
+};
--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,11 @@
-import express from 'express';
 import mongoose from 'mongoose';
-import productRouter from './product.routes';
+import { startHTTPApi } from './api';
 
 const connectMongoDb = async (): Promise<void> => {
   await mongoose.connect('mongodb://admin:admin123@localhost:27017/db?authSource=admin');
   console.log('Mongodb connected!');
 };
 
-const startHTTPApi = (): void => {
-  const app = express();
-
-  // Middleware para decirle a Express que los body serán JSON (método POST)
-  app.use(express.json());
-
-  app.use('/products', productRouter);
-  app.use('/products/:productId', productRouter);
-
-  app.listen(3000, () => {
-    console.log('Up & running on port: ', 3000);
-  });
-};
 const executeApp = async (): Promise<void> => {
   try {
     await connectMongoDb();
