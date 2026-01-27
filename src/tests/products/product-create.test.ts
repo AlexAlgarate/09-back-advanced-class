0750cca2ed1f7af1e45d6928ab34c3c7eab89814
Alex Algarate <alex.algarate@gmail.com>
2026-01-27 21:49:40 +0100

    test POST endpoint

---
+++ src/tests/products/product-create.test.ts
@@ -0,0 +1,23 @@
+import request from 'supertest';
+import { app } from '../../api';
+
+describe('GET /products', () => {
+  it('Given no name or description, should return a 400 error', async () => {
+    const productError = { name: 'test-error' };
+
+    const response = await request(app).post('/products').send(productError);
+
+    expect(response.status).toBe(400);
+    expect(response.body).toStrictEqual({ message: 'name and description have to be dedfined' });
+  });
+  it('Product should be created (201)', async () => {
+    const product = { name: 'test', description: 'test' };
+
+    const response = await request(app).post('/products').send(product);
+
+    expect(response.status).toBe(201);
+    expect(response.body).toMatchObject({
+      content: { name: 'test', description: 'test' },
+    });
+  });
+});
