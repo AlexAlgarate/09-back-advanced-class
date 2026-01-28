7cf7e2a535ab1270c07f8b1078bc58fd27dcb90f
Alex Algarate <alex.algarate@gmail.com>
2026-01-28 22:39:44 +0100

    add more variables to Product domain entity

--- src/domain/entities/Product.ts
+++ src/domain/entities/Product.ts
@@ -1,9 +1,25 @@
 export class Product {
   readonly name;
   readonly description;
+  readonly id;
+  readonly createdAt;
 
-  constructor({ name, description }: { name: string; description: string }) {
+  constructor({
+    name,
+    description,
+    id,
+    createdAt,
+  }: {
+    name: string;
+    description: string;
+
+    id: string;
+    createdAt: Date;
+  }) {
+    this.id = id;
     this.name = name;
+    this.createdAt = createdAt;
+
     this.description = description;
   }
 
