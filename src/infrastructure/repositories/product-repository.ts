d347c094670582527e93ee2f0fd294d1440a2097
Alex Algarate <alex.algarate@gmail.com>
2026-01-28 22:41:10 +0100

    Create product mongo repository infra

---
+++ src/infrastructure/repositories/product-repository.ts
@@ -0,0 +1,20 @@
+import { Product } from '../../domain/entities/Product';
+import { ProductRepository } from '../../domain/repositories/ProductRepository';
+import { ProductModel } from '../models/product-models';
+
+export class ProductMongodbRepository implements ProductRepository {
+  async createOne({ name, description }: { name: string; description: string }): Promise<Product> {
+    const newProduct = new ProductModel({
+      name,
+      description,
+    });
+
+    const createdProduct = await newProduct.save();
+    return new Product({
+      id: createdProduct._id.toString(),
+      name: createdProduct.name,
+      description: createdProduct.description,
+      createdAt: createdProduct.createdAt,
+    });
+  }
+}
