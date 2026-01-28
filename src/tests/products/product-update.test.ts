62839e377ecc96ff4ba123264f25a0a1813b07bd
Alex Algarate <alex.algarate@gmail.com>
2026-01-28 11:37:01 +0100

    feat: refactor test PATCH endpoint using createRandomProduct helper

--- src/tests/products/product-update.test.ts
+++ src/tests/products/product-update.test.ts
@@ -1,5 +1,6 @@
 import request from 'supertest';
 import { app } from '../../api';
+import { createRandomProduct } from './helpers';
 
 describe('PATCH /products/:productId', () => {
   it('Should return a 400 if product does not exist', async () => {
@@ -10,10 +11,7 @@ describe('PATCH /products/:productId', () => {
   });
 
   it('Should return a updated product', async () => {
-    const product = await request(app).post('/products').send({
-      name: 'test',
-      description: 'test-description',
-    });
+    const product = await createRandomProduct();
 
     const productId = product.body.content._id;
 
@@ -25,7 +23,7 @@ describe('PATCH /products/:productId', () => {
     expect(response.body).toMatchObject({
       content: {
         name: 'updated-name',
-        description: 'test-description',
+        description: product.body.content.description,
         __v: 0,
       },
     });
