d055077f82753253c79784f9ce8382dbd63a80d3
Alex Algarate <alex.algarate@gmail.com>
2026-01-28 22:40:19 +0100

    create product repository interface domain

---
+++ src/domain/repositories/ProductRepository.ts
@@ -0,0 +1,5 @@
+import { Product } from '../entities/Product';
+
+export interface ProductRepository {
+  createOne({ name, description }: { name: string; description: string }): Promise<Product>;
+}
