d6235c7af279bad1e17d8746d709d6759d091e0f
Alex Algarate <alex.algarate@gmail.com>
2026-01-28 22:41:28 +0100

    create product controller

---
+++ src/ui/controllers/products/create-product-controller.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { CreateProductUseCase } from '../../../domain/use-cases/product/create-product-usecase';
+import { ProductMongodbRepository } from '../../../infrastructure/repositories/product-repository';
+
+export const createProductController = async (request: Request, response: Response) => {
+  // TODO fix
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const { name, description } = request.body;
+
+  if (!name || !description) {
+    response.status(400).json({
+      message: 'name and description have to be dedfined',
+    });
+  }
+  // inyectamos el caso de uso con un repositorio
+  const productRepository = new ProductMongodbRepository();
+  const createProductUseCase = new CreateProductUseCase(productRepository);
+
+  // TODO fix
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  const createdProduct = await createProductUseCase.execute({ name, description });
+
+  return response.status(201).json({ content: createdProduct });
+};
