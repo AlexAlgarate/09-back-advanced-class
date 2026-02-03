0e768e339dd885ef917228151620cadac7ae161a
Alex Algarate <alex.algarate@gmail.com>
2026-02-03 09:34:07 +0100

    use Zod in delete product controller

--- src/ui/controllers/products/delete-product-controller.ts
+++ src/ui/controllers/products/delete-product-controller.ts
@@ -1,22 +1,32 @@
 import { Request, Response } from 'express';
+import * as z from 'zod';
 
 import { ProductMongodbRepository } from '@infrastructure/repositories/product-mongo-repository';
 import { DeleteProductUseCase } from '@domain/use-cases/product/delete-product-usecase';
 
+const deleteProductParamsValidator = z.object({
+  productId: z.string(),
+});
+
+const userRequestValidator = z.object({
+  id: z.string(),
+});
+
 export const deleteProductController = async (
-  request: Request<{ productId: string }>,
+  request: Request,
   response: Response
 ): Promise<void> => {
-  const { productId } = request.params;
+  const { productId } = deleteProductParamsValidator.parse(request.params);
+  const { id: userId } = userRequestValidator.parse(request.user);
 
   const productMongodbRepository = new ProductMongodbRepository();
   const deleteProductUseCase = new DeleteProductUseCase(productMongodbRepository);
 
   try {
-    await deleteProductUseCase.execute(productId, request.user?.id as string);
+    await deleteProductUseCase.execute(productId, userId);
     response.json({ message: 'Product removed successfully' });
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
   } catch (error) {
-    response.status(400).json({ message: 'Product not found' });
+    response.status(404).json({ message: 'Product not found' });
   }
 };
